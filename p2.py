f801f96ad905f00647bb2a0147482095e8d61e50
Devansh Tayal <devanshtayal2005@gmail.com>
2020-09-23 22:46:15 +0530

    Trial

--- p2.py
+++ p2.py
@@ -1,4 +1,4 @@
 import math
 number = float(input("Enter the number: "))
-sqrt = math.sqrt(number)
-print("Square root of", number ,"is", sqrt)
+sq = math.sqrt(number)
+print("Square root of", number ,"is", sq)
